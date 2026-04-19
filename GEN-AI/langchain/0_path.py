a07704b2707a457c1d9c017b0896a655d902ee43
Ganesh-Korada1193 <ganeshnotes113@gmail.com>
2026-04-19 12:38:05 +0530

    model and message files created

---
+++ GEN-AI/langchain/0_path.py
@@ -0,0 +1,12 @@
+#_________________________________________________
+
+# -------> messages ------> model
+
+'''
+1. what is model?
+2. initialize the model?
+3. ---------INPUT-------
+    a. format: string, message
+    b. ways: invoke, stream, batch
+'''
+#_________________________________________________
